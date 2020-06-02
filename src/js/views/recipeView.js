71345711fab2ae771fa8ae2e1056bb29c971fdc3
Katja Stefan <katja.stefan18@gmail.com>
2020-06-02 12:38:54 +0200

    Installing Fractional package and creating a fn that will format the count

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,29 @@
 import { elements } from './base';
+import { Fraction } from 'fractional';
 
 export const clearRecipe = () => {
     elements.recipe.innerHTML = '';
+};
+
+const formatCount = count => {
+    // Return a formatted string
+    if (count) {
+        //  count = 2.5 => 2 1/2
+        // First seaprate integer part from decimal part
+        const [int, dec] = count.toString().split('.').map(el => parseInt(el, 10));
+
+        if (!dec) return count;
+
+        if (int === 0) {
+            // Based on the count it will now create a fraction and from there we can read the numerator and denominator
+            const fr = new Fraction(count);
+            return `${fr.numerator}/${fr.denominator}`;
+        } else {
+            const fr = new Fraction(count - int);
+            return `${int} ${fr.numerator}/${fr.denominator}`;
+        }
+    }
+    return '?';
 };
 
 const createIngredient = ingredient => `
@@ -9,7 +31,7 @@ const createIngredient = ingredient => `
         <svg class="recipe__icon">
             <use href="img/icons.svg#icon-check"></use>
         </svg>
-        <div class="recipe__count">${ingredient.count}</div>
+        <div class="recipe__count">${formatCount(ingredient.count)}</div>
         <div class="recipe__ingredient">
             <span class="recipe__unit">${ingredient.unit}</span>
             ${ingredient.ingredient}
